20ea2dd5c4c0b1e2e3a8b13fe694616524d98166
Wajdwael <wajdabdelhai@gmail.com>
2024-12-19 09:58:34 +0300

    Initial Commit

---
+++ challenge4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  challenge4
+//
+//  Created by Wajd Wael on 18/06/1446 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ challenge4/challenge4App.swift
@@ -0,0 +1,17 @@
+//
+//  challenge4App.swift
+//  challenge4
+//
+//  Created by Wajd Wael on 18/06/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct challenge4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
